87184e4e9aa23b4888bb0d4049aa456efded6618
Felipe Nascimento <flpnascto@gmail.com>
2020-11-12 11:20:19 -0300

    feat: component conteudo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import Conteudo from './components/Conteudo';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Conteudo />
     </div>
   );
 }
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Conteudo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ó | INCIPIENTIUM</h1>
+        <h2>Porque invertir é para todos</h2>
+        <a href="https://minhaconta.orama.com.br/novo-cadastro">ABRA SUA CONTA</a>
+      </div>
+    );
+  }
+}
+
+export default Conteudo;
