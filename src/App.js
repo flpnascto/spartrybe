963e68584aa09a22f93642445ce824fe7ea3326f
kramersilva <kramerscs@gmail.com>
2020-11-14 11:22:27 -0300

    InvestLibrary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Menu from './components/Menu';
 import Conteudo from './components/Conteudo';
-import Header from './components/Header'
+import Header from './components/Header';
+import InvestLibrary from './components/InvestLibrary';
 
+import Portfolio from './data'
+console.log(Portfolio)
 function App() {
   return (
     <div className="App">
       <Header />
+      <InvestLibrary portfolio={Portfolio}/>
       <Menu />
       <Conteudo />
     </div>
