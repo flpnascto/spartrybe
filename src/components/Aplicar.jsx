815dba472fc5df03ed4f402c72fcafd77befb2ca
kramersilva <kramerscs@gmail.com>
2020-11-14 21:12:30 -0300

    feat aplicar

---
+++ src/components/Aplicar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../styles/Aplicar.css'
+
+
+class Aplicar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      valor: '',
+    }
+  }
+
+  handleClick = (event) => {
+    alert('Aplicação realizada com sucesso!')
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target
+
+    this.setState((previous) => ({
+      valor: value,
+    }))
+  }
+  render() {
+    const { valor } = this.state
+    return(
+      <div className="invest-Container">
+        <label className="invest-label" htmlFor="input-value">
+          <h3>Valor</h3>
+          <input id="input-value" value={this.state.valor} onChange={this.handleChange} className="input-value"  /></label>
+          <button className="invest-button" disabled={valor <= 0} onClick={this.handleClick}>Investir</button>
+      </div>
+    )
+  }
+}
+
+export default Aplicar;
